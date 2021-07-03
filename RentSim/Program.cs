f3b784065dd61062bac81cd849395c038882bf57
Donnie Sunset <Donnie.Sunset@gmail.com>
2021-07-03 21:10:46 +0200

    Included Blazorise

--- RentSim/Program.cs
+++ RentSim/Program.cs
@@ -1,3 +1,6 @@
+using Blazorise;
+using Blazorise.Bootstrap;
+using Blazorise.Icons.FontAwesome;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -14,12 +17,33 @@ namespace RentSim
     {
         public static async Task Main(string[] args)
         {
+            //var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            //builder.RootComponents.Add<App>("#app");
+
+            //builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            //await builder.Build().RunAsync();
+
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+            builder.Services
+              .AddBlazorise(options =>
+              {
+                  options.ChangeTextOnKeyPress = true;
+              })
+              .AddBootstrapProviders()
+              .AddFontAwesomeIcons();
+
+            builder.Services.AddSingleton(new HttpClient
+            {
+                BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
+            });
+
             builder.RootComponents.Add<App>("#app");
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            var host = builder.Build();
 
-            await builder.Build().RunAsync();
+            await host.RunAsync();
         }
     }
 }
