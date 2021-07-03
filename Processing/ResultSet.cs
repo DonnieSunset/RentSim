b9128fc257ce3b5afd70ca8f33eb5f25a2fed2d1
Donnie Sunset <Donnie.Sunset@gmail.com>
2021-07-03 22:03:41 +0200

    Connected UI with input

---
+++ Processing/ResultSet.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Processing
+{
+    public class ResultSet
+    {
+        public ResultSet()
+        {
+            Total = new List<float>();
+        }
+
+        public List<float> Total;
+    }
+}
