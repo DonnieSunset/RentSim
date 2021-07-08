2b058b5a423a69dc538673963ee06008e257c99b
Donnie Sunset <Donnie.Sunset@gmail.com>
2021-07-08 23:36:57 +0200

    First Walking Skeleton

--- Processing/Processing.cs
+++ Processing/Processing.cs
@@ -4,17 +4,17 @@ namespace Processing
 {
     public class Calc
     {
-        public ResultSet DoCalculation(int years, int startCapital, float interestRate)
-        {
-            ResultSet result = new ResultSet();
-            result.Total.Add(startCapital);
+        //public ResultSet DoCalculation(int years, int startCapital, float interestRate)
+        //{
+        //    ResultSet result = new ResultSet();
+        //    result.Total.Add(startCapital);
 
-            for (int i = 0; i < years; i++)
-            {
-                result.Total.Add(result.Total[i] + result.Total[i] * interestRate);
-            }
+        //    for (int i = 0; i < years; i++)
+        //    {
+        //        result.Total.Add(result.Total[i] + result.Total[i] * interestRate);
+        //    }
 
-            return result;
-        }
+        //    return result;
+        //}
     }
 }
--- Processing/ResultSet.cs
+++ Processing/ResultSet.cs
@@ -6,13 +6,55 @@ using System.Threading.Tasks;
 
 namespace Processing
 {
+    public class Input
+    {
+        public int ageCurrent;
+        public int ageStopWork;
+
+        public int stocks;
+        public int stocksInterestRate;
+        public int stocksSaveAmount;
+    }
+
+    public class Snapshot
+    {
+        public int age;
+        public double stocksYearBegin;
+        public double stocksInvests;
+        public double stocksInterests;
+        public double stocksYearEnd;
+    }
+
     public class ResultSet
     {
-        public ResultSet()
-        {
-            Total = new List<float>();
-        }
+        //public ResultSet()
+        //{
+        //    Total = new List<Snapshot>();
+        //}
 
-        public List<float> Total;
+        //public List<Snapshot> Total;
+
+        public List<Snapshot> Process(Input _input)
+        {
+            Snapshot _curSnap = null;
+            Snapshot _lastSnap = null;
+
+            var resultSet = new List<Snapshot>();
+
+            for (int i = _input.ageCurrent; i < _input.ageStopWork; i++)
+            {
+                _curSnap = new Snapshot();
+                _curSnap.age = i;
+                _curSnap.stocksYearBegin = i == _input.ageCurrent ? _input.stocks : _lastSnap.stocksYearEnd;
+                _curSnap.stocksInvests = _input.stocksSaveAmount * 12;
+                _curSnap.stocksInterests = (_curSnap.stocksYearBegin + _curSnap.stocksInvests) * _input.stocksInterestRate / 100;
+                _curSnap.stocksYearEnd = _curSnap.stocksYearBegin + _curSnap.stocksInvests + _curSnap.stocksInterests;
+
+                resultSet.Add(_curSnap);
+                _lastSnap = _curSnap;
+            }
+
+            return resultSet;
+        }
     }
 }
