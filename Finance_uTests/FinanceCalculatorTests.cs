1131e83e28c10a5215b51a80e9f41aaecee3115a
Donnie Sunset <Donnie.Sunset@gmail.com>
2022-05-18 20:49:22 +0200

    added a prototype test case for calculating the rent phase needs

--- Finance_uTests/FinanceCalculatorTests.cs
+++ Finance_uTests/FinanceCalculatorTests.cs
@@ -17,5 +17,52 @@ namespace Finance_uTests
             Assert.That(result1, Is.EqualTo(expectedPartAmount1).Within(0.000000000000001));
             Assert.That(result2, Is.EqualTo(expectedPartAmount2).Within(0.000000000000001));
         }
+
+        [Test]
+        public void Blubb()
+        {
+            double stocks_interestRate_goodCase = 1.07;
+            double cash_interestRate_goodCase = 1.01;
+            double stocks_interestRate_badCase = 1.0;
+            double cash_interestRate_badCase = 1.0;
+            int years = 13;
+
+            double stocks_crashFactor_badCase = 0.5;
+
+            double minimum_total_needed = 2000 * 12 * years;
+            double comfort_total_needed = 3000 * 12 * years;
+
+            double rent = 1690 * 12 * years;
+
+            double cash_needed;
+            double stocks_needed;
+
+            double stocks_interestFactor_goodCase = Math.Pow(stocks_interestRate_goodCase, 13);
+            double cash_interestFactor_goodCase = Math.Pow(cash_interestRate_goodCase, 13);
+            double stocks_interestFactor_badCase = Math.Pow(stocks_interestRate_badCase, 13);
+            double cash_interestFactor_badCase = Math.Pow(cash_interestRate_badCase, 13);
+
+            //(1) 3000 = rente + (festgeld * 1.00^13) + (aktien * 1.00^13 * 0.5)
+            //(2) 5000 = rente + (festgeld * 1.01^13) + (aktien * 1.08^13)
+
+            //(1) minimum_total_needed = rent + (cash_needed * cash_interestFactor_badCase) + (stocks_needed * stocks_interestFactor_badCase * stocks_crashFactor_badCase)
+            //(2) comfort_total_needed = rent + (cash_needed * cash_interestFactor_goodCase) + (stocks_needed * stocks_interestFactor_goodCase)
+
+            // py https://live.sympy.org/
+            // minimum_total_needed, rent, cash_needed, cash_interestFactor_badCase, stocks_needed, stocks_interestFactor_badCase, stocks_crashFactor_badCase, comfort_total_needed, cash_interestFactor_goodCase, stocks_interestFactor_goodCase = symbols('minimum_total_needed rent cash_needed cash_interestFactor_badCase stocks_needed stocks_interestFactor_badCase stocks_crashFactor_badCase comfort_total_needed cash_interestFactor_goodCase stocks_interestFactor_goodCase')
+            // eq1 = Eq(rent + (cash_needed * cash_interestFactor_badCase) + (stocks_needed * stocks_interestFactor_badCase * stocks_crashFactor_badCase), minimum_total_needed)
+            // eq2 = Eq(rent + (cash_needed * cash_interestFactor_goodCase) + (stocks_needed * stocks_interestFactor_goodCase), comfort_total_needed)
+            // sol = solve((eq1, eq2),(cash_needed, stocks_needed))
+            //
+            // 
+
+            cash_needed = (-stocks_crashFactor_badCase * stocks_interestFactor_badCase * (comfort_total_needed - rent) + stocks_interestFactor_goodCase * (minimum_total_needed - rent)) / (cash_interestFactor_badCase * stocks_interestFactor_goodCase - cash_interestFactor_goodCase * stocks_crashFactor_badCase * stocks_interestFactor_badCase);
+            stocks_needed = (cash_interestFactor_badCase*(comfort_total_needed - rent) - cash_interestFactor_goodCase*(minimum_total_needed - rent)) / (cash_interestFactor_badCase*stocks_interestFactor_goodCase - cash_interestFactor_goodCase*stocks_crashFactor_badCase*stocks_interestFactor_badCase);
+
+            Console.WriteLine($"cash_needed: {cash_needed}");
+            Console.WriteLine($"stocks_needed: {stocks_needed}");
+
+            Assert.True(true);
+        }
     }
 }
--- Strategy/RentStartStrategy.cs
+++ Strategy/RentStartStrategy.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Portfolio;
+using Protocol;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,7 +22,7 @@ namespace Strategy
     // 2 gleichungen mit 2 unbekannten -> lösbar
 
     //3000 = rente + festgeld + (aktien* 0.5)
-    //5000 = rente + festgeld + aktien
+    //5000 = rente + festgeld + aktien * 1.08^13
 
     //2500 = festgeld + 0.5* aktien
     //4500 = festgeld + aktien
@@ -32,7 +34,39 @@ namespace Strategy
     //4000 = aktien
     //500 = festgeld
 
-    internal class RentStartStrategy
+    //todo: auch zinsberechung auf festgeld + rente
+
+    public class RentStartStrategy
     {
+        private AssetPortfolio myPortfolio;
+        private IProtocolWriter myProtocolWriter;
+        private LifeAssumptions myLifeAssumptions;
+
+        public RentStartStrategy(AssetPortfolio portfolio, LifeAssumptions lifeAssumptions, IProtocolWriter protocolWriter)
+        {
+            myPortfolio = portfolio;
+            myLifeAssumptions = lifeAssumptions;
+            myProtocolWriter = protocolWriter;
+        }
+
+        public void Process(decimal neededMonthlyAmount)
+        {
+            //initial cash amount
+            //myProtocolWriter.LogBalanceYearBegin(
+            //    Age.NewByAbsoluteAge(myLifeAssumptions.ageCurrent),
+            //    myLifeAssumptions.cash
+            //    );
+
+            for (int i = myLifeAssumptions.ageRentStart; i < myLifeAssumptions.ageEnd; i++)
+            {
+                Age age = Age.NewByAbsoluteAge(i);
+
+                TransactionDetails transAction = myPortfolio.WithdrawCash(neededMonthlyAmount * 12);
+                myProtocolWriter.Log(age, transAction);
+
+                transAction = myPortfolio.GetInterestsForCash();
+                myProtocolWriter.Log(age, transAction);
+            }
+        }
     }
 }
