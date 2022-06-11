75d6ef6b8f2f4eb6132b7e40740e5d27f9db70bf
Donnie Sunset <Donnie.Sunset@gmail.com>
2022-06-11 12:54:09 +0200

    fixed small bug in RentCalculator.ApproxNetRent

--- Finance/RentCalculator.cs
+++ Finance/RentCalculator.cs
@@ -4,9 +4,9 @@
     {
         public static decimal ApproxNetRent(int ageCurrent, decimal netRentAgeCurrent, int ageRentStart, decimal netRentAgeRentStart, int ageInQuestion)
         {
-            if (ageInQuestion <= ageCurrent || ageInQuestion >= netRentAgeRentStart)
+            if (ageInQuestion <= ageCurrent || ageInQuestion >= ageRentStart)
             {
-                throw new InvalidDataException($"Param: {nameof(ageInQuestion)}");
+                throw new InvalidDataException($"Param: {nameof(ageInQuestion)} is {ageInQuestion} but should be between {ageCurrent} and {ageRentStart}.");
             }
 
             var result = (netRentAgeRentStart - netRentAgeCurrent) / (ageRentStart - ageCurrent) * (ageInQuestion - ageCurrent) + netRentAgeCurrent;
