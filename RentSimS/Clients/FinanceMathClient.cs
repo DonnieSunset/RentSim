b6e382c8b6a51929d33347d183d03e35f9517767
Donnie Sunset <Donnie.Sunset@gmail.com>
2023-01-23 13:07:27 +0100

    included stop work phase in Index.razor

--- RentSimS/Clients/FinanceMathClient.cs
+++ RentSimS/Clients/FinanceMathClient.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Blazorise;
+using Microsoft.AspNetCore.Mvc;
 using System.Globalization;
 
 namespace RentSimS.Clients
@@ -41,17 +42,17 @@ namespace RentSimS.Clients
             }
         }
 
-        public async Task<decimal> RateByNumericalSparkassenformel(double betrag1, double zins1, double betrag2, double zins2, double betrag3, double zins3, double endbetrag, int yearStart, int yearEnd)
+        public async Task<string> RateByNumericalSparkassenformel(decimal betrag_cash, decimal betrag_stocks, decimal betrag_metals, decimal zins_cash, decimal zins_stocks, decimal zins_metals, decimal endbetrag, int yearStart, int yearEnd)
         {
             var ub = new UriBuilder(myUrl);
             ub.Path = "FinanceMath/RateByNumericalSparkassenformel";
             ub.Query =
-                $"?betrag1={betrag1.ToString(CultureInfo.InvariantCulture)}" +
-                $"&zins1={zins1.ToString(CultureInfo.InvariantCulture)}" +
-                $"&betrag2={betrag2.ToString(CultureInfo.InvariantCulture)}" +
-                $"&zins2={zins2.ToString(CultureInfo.InvariantCulture)}" +
-                $"&betrag3={betrag3.ToString(CultureInfo.InvariantCulture)}" +
-                $"&zins3={zins3.ToString(CultureInfo.InvariantCulture)}" +
+                $"?betrag_cash={betrag_cash.ToString(CultureInfo.InvariantCulture)}" +
+                $"&betrag_stocks={betrag_stocks.ToString(CultureInfo.InvariantCulture)}" +
+                $"&betrag_metals={betrag_metals.ToString(CultureInfo.InvariantCulture)}" +
+                $"&zins_cash={zins_cash.ToString(CultureInfo.InvariantCulture)}" +
+                $"&zins_stocks={zins_stocks.ToString(CultureInfo.InvariantCulture)}" +
+                $"&zins_metals={zins_metals.ToString(CultureInfo.InvariantCulture)}" +
                 $"&endbetrag={endbetrag.ToString(CultureInfo.InvariantCulture)}" +
                 $"&yearStart={yearStart}" +
                 $"&yearEnd={yearEnd}"
@@ -71,14 +72,9 @@ namespace RentSimS.Clients
                     throw new Exception($"{nameof(stringResponse)} is null.");
                 }
 
-                decimal result = decimal.Parse(stringResponse, CultureInfo.InvariantCulture);
-
-                return result;
+                return stringResponse;
             }
         }
-
-//        public JsonResult StartCapitalByNumericalSparkassenformel(decimal rateTotal_perYear, double factor1, double zins1, double factor2, double zins2, double factor3, double zins3, decimal endbetrag, int yearStart, int yearEnd)
-
 
         public async Task<string> StartCapitalByNumericalSparkassenformel(decimal rateTotal_perYear, double factor_cash, double zins_cash, double factor_stocks, double zins_stocks, double factor_metals, double zins_metals, decimal endbetrag, int yearStart, int yearEnd)
         {
@@ -106,13 +102,10 @@ namespace RentSimS.Clients
                 }
 
                 var stringResponse = await response.Content.ReadAsStringAsync();
-                //var stringResponse = await response.Content.ReadFromJsonAsync();
                 if (stringResponse == null)
                 {
                     throw new Exception($"{nameof(stringResponse)} is null.");
                 }
-
-                //decimal result = decimal.Parse(stringResponse, CultureInfo.InvariantCulture);
 
                 return stringResponse;
             }
