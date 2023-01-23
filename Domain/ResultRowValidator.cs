0fe9569ad0203abe4303fb8dda9dda065395cd6b
Donnie Sunset <Donnie.Sunset@gmail.com>
2023-01-23 18:31:37 +0100

    small fix in validator

--- Domain/ResultRowValidator.cs
+++ Domain/ResultRowValidator.cs
@@ -2,6 +2,8 @@
 {
     public class ResultRowValidator
     {
+        private static int roundingAccuracy = 2;
+
         public static void ValidateAll(IEnumerable<ResultRow> resultRows, int ageCurrent, int ageStopWork, int ageEnd/*, decimal stocksTaxFactor*/)
         {
             AllAgesAvailable(resultRows, ageCurrent, ageEnd);
@@ -35,22 +37,22 @@
                 var current = resultRows.Single(x => x.age == i);
                 var next = resultRows.Single(x => x.age == i + 1);
 
-                if (Decimal.Round(current.cashYearEnd, 3) != Decimal.Round(next.cashYearBegin, 3))
+                if (Decimal.Round(current.cashYearEnd, roundingAccuracy) != Decimal.Round(next.cashYearBegin, roundingAccuracy))
                 {
                     throw new Exception($"ResultRowValidator: Cash YearEnd and YearBegin mismatch at index {i} ({current.cashYearEnd}) and {i + 1} ({next.cashYearBegin}).");
                 }
 
-                if (Decimal.Round(current.stocksYearEnd, 3) != Decimal.Round(next.stocksYearBegin, 3))
+                if (Decimal.Round(current.stocksYearEnd, roundingAccuracy) != Decimal.Round(next.stocksYearBegin, roundingAccuracy))
                 {
                     throw new Exception($"ResultRowValidator: Stocks YearEnd and YearBegin mismatch at index {i} ({current.stocksYearEnd}) and {i + 1} ({next.stocksYearBegin}).");
                 }
 
-                if (Decimal.Round(current.metalsYearEnd, 3) != Decimal.Round(next.metalsYearBegin, 3))
+                if (Decimal.Round(current.metalsYearEnd, roundingAccuracy) != Decimal.Round(next.metalsYearBegin, roundingAccuracy))
                 {
                     throw new Exception($"ResultRowValidator: Metals YearEnd and YearBegin mismatch at index {i} ({current.metalsYearEnd}) and {i + 1} ({next.metalsYearBegin}).");
                 }
 
-                if (Decimal.Round(current.TotalYearEnd, 3) != Decimal.Round(next.TotalYearBegin, 3))
+                if (Decimal.Round(current.TotalYearEnd, roundingAccuracy) != Decimal.Round(next.TotalYearBegin, roundingAccuracy))
                 {
                     throw new Exception($"ResultRowValidator: Total YearEnd and YearBegin mismatch at index {i} ({current.TotalYearEnd}) and {i + 1} ({next.TotalYearBegin}).");
                 }
@@ -61,7 +63,7 @@
         {
             var totalYearEnd = resultRows.Single(x => x.age == ageEnd-1).TotalYearEnd;
 
-            if (Decimal.Round(totalYearEnd, 3) != 0)
+            if (Decimal.Round(totalYearEnd, roundingAccuracy) != 0)
             {
                 throw new Exception($"ResultRowValidator: Last row does not end up in zero. Actual value: {totalYearEnd}.");
             }
@@ -72,22 +74,22 @@
             foreach (var resultRow in resultRows)
             {
                 if (resultRow.age < 0 ||
-                    Decimal.Round(resultRow.cashInterests, 3) < 0 ||
-                    Decimal.Round(resultRow.cashTaxes, 3) > 0 ||
-                    Decimal.Round(resultRow.cashYearBegin, 3) < 0 ||
-                    Decimal.Round(resultRow.cashYearEnd, 3) < 0 ||
-                    Decimal.Round(resultRow.stocksInterests, 3) < 0 ||
-                    Decimal.Round(resultRow.stocksTaxes, 3) > 0 ||
-                    Decimal.Round(resultRow.stocksYearBegin, 3) < 0 ||
-                    Decimal.Round(resultRow.stocksYearEnd, 3) < 0 ||
-                    Decimal.Round(resultRow.metalsInterests, 3) < 0 ||
-                    Decimal.Round(resultRow.metalsTaxes, 3) > 0 ||
-                    Decimal.Round(resultRow.metalsYearBegin, 3) < 0 ||
-                    Decimal.Round(resultRow.metalsYearEnd, 3) < 0 ||
-                    Decimal.Round(resultRow.TotalInterests, 3) < 0 ||
-                    Decimal.Round(resultRow.TotalTaxes, 3) > 0 ||
-                    Decimal.Round(resultRow.TotalYearBegin, 3) < 0 ||
-                    Decimal.Round(resultRow.TotalYearEnd, 3) < 0)
+                    Decimal.Round(resultRow.cashInterests, roundingAccuracy) < 0 ||
+                    Decimal.Round(resultRow.cashTaxes, roundingAccuracy) > 0 ||
+                    Decimal.Round(resultRow.cashYearBegin, roundingAccuracy) < 0 ||
+                    Decimal.Round(resultRow.cashYearEnd, roundingAccuracy) < 0 ||
+                    Decimal.Round(resultRow.stocksInterests, roundingAccuracy) < 0 ||
+                    Decimal.Round(resultRow.stocksTaxes, roundingAccuracy) > 0 ||
+                    Decimal.Round(resultRow.stocksYearBegin, roundingAccuracy) < 0 ||
+                    Decimal.Round(resultRow.stocksYearEnd, roundingAccuracy) < 0 ||
+                    Decimal.Round(resultRow.metalsInterests, roundingAccuracy) < 0 ||
+                    Decimal.Round(resultRow.metalsTaxes, roundingAccuracy) > 0 ||
+                    Decimal.Round(resultRow.metalsYearBegin, roundingAccuracy) < 0 ||
+                    Decimal.Round(resultRow.metalsYearEnd, roundingAccuracy) < 0 ||
+                    Decimal.Round(resultRow.TotalInterests, roundingAccuracy) < 0 ||
+                    Decimal.Round(resultRow.TotalTaxes, roundingAccuracy) > 0 ||
+                    Decimal.Round(resultRow.TotalYearBegin, roundingAccuracy) < 0 ||
+                    Decimal.Round(resultRow.TotalYearEnd, roundingAccuracy) < 0)
                 {
                     throw new Exception($"ResultRowValidator: row of age {resultRow.age} contains an entry with wrong sign.");
                 }
@@ -98,10 +100,10 @@
         {
             foreach (var resultRow in resultRows)
             {
-                bool eqCash = Decimal.Round(resultRow.cashYearBegin + resultRow.cashDeposits.Sum() + resultRow.cashInterests + resultRow.cashTaxes, 3) == Decimal.Round(resultRow.cashYearEnd, 3);
-                bool eqStocks = Decimal.Round(resultRow.stocksYearBegin + resultRow.stocksDeposits.Sum() + resultRow.stocksInterests + resultRow.stocksTaxes, 3) == Decimal.Round(resultRow.stocksYearEnd, 3);
-                bool eqMetals = Decimal.Round(resultRow.metalsYearBegin + resultRow.metalsDeposits.Sum() + resultRow.metalsInterests + resultRow.metalsTaxes, 3) == Decimal.Round(resultRow.metalsYearEnd, 3);
-                bool eqTotals = Decimal.Round(resultRow.TotalYearBegin + resultRow.TotalDeposits + resultRow.TotalInterests + resultRow.TotalTaxes, 3) == Decimal.Round(resultRow.TotalYearEnd, 3);
+                bool eqCash = Decimal.Round(resultRow.cashYearBegin + resultRow.cashDeposits.Sum() + resultRow.cashInterests + resultRow.cashTaxes, roundingAccuracy) == Decimal.Round(resultRow.cashYearEnd, roundingAccuracy);
+                bool eqStocks = Decimal.Round(resultRow.stocksYearBegin + resultRow.stocksDeposits.Sum() + resultRow.stocksInterests + resultRow.stocksTaxes, roundingAccuracy) == Decimal.Round(resultRow.stocksYearEnd, roundingAccuracy);
+                bool eqMetals = Decimal.Round(resultRow.metalsYearBegin + resultRow.metalsDeposits.Sum() + resultRow.metalsInterests + resultRow.metalsTaxes, roundingAccuracy) == Decimal.Round(resultRow.metalsYearEnd, roundingAccuracy);
+                bool eqTotals = Decimal.Round(resultRow.TotalYearBegin + resultRow.TotalDeposits + resultRow.TotalInterests + resultRow.TotalTaxes, roundingAccuracy) == Decimal.Round(resultRow.TotalYearEnd, roundingAccuracy);
 
                 if (!eqCash || !eqStocks || !eqMetals || !eqTotals)
                 {
@@ -129,8 +131,8 @@
                 
                 if (stocksDeposits < 0)
                 {
-                    var actualTaxes = decimal.Round(resultRow.stocksTaxes, 3);
-                    var assumedTaxes = decimal.Round(stocksDeposits * (stocksTaxFactor - 1), 3);
+                    var actualTaxes = decimal.Round(resultRow.stocksTaxes, roundingAccuracy);
+                    var assumedTaxes = decimal.Round(stocksDeposits * (stocksTaxFactor - 1), roundingAccuracy);
                     if (actualTaxes != assumedTaxes)
                     {
                         throw new Exception($"ResultRowValidator: Taxes for stocks deposits of {stocksDeposits} at age {resultRow.age} are {actualTaxes} but should be {assumedTaxes}.");
